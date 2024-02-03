649534f770c75d93f999a5d907f6955c078a84e4
issaali <issa.alali.991@gmail.com>
2024-02-03 14:53:34 +0100

    modify API_link for development satus

--- Frontend/src/usersLogic.js
+++ Frontend/src/usersLogic.js
@@ -1,19 +1,16 @@
-
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
 
-export const getUser = async (
-  user,
-  pass,
-  setCookie
+export const getUser = async (user, pass, setCookie) => {
+  console.log(API_BASE_URL);
 
-) => {
   try {
-    const res = await axios.post("https://authentication-form-u8no.onrender.com/users/login", {
+    const res = await axios.post(`${API_BASE_URL}/users/login`, {
       user_name: user,
       password: pass,
     });
-    setCookie('access_token',res.data.token);
+    setCookie('access_token', res.data.token);
     window.localStorage.setItem('userID', res.data.id);
     window.localStorage.setItem('user', user);
     console.log(res.data);
@@ -30,25 +27,22 @@ export const getUser = async (
     }
   }
 };
- export const createUser = async (
-  user,
-  pass,
-  setCookie
-) => {
+
+export const createUser = async (user, pass, setCookie) => {
+  console.log(API_BASE_URL);
   try {
-    const res = await axios.post("https://authentication-form-u8no.onrender.com/users/register", {
+    const res = await axios.post(`${API_BASE_URL}/register`, {
       user_name: user,
       password: pass,
     });
-    const {token,id,message} = res.data;
-    setCookie('access_token',token);
+    const { token, id, message } = res.data;
+    setCookie('access_token', token);
     window.localStorage.setItem('userID', id);
-    res.data.message==="User created successfully" && (
-      alert(message),
-      getUser(user,pass,setCookie)
-
-    );
-     
+    
+    if (message === "User created successfully") {
+      alert(message);
+      getUser(user, pass, setCookie);
+    }
   } catch (error) {
     console.error("Error creating user:", error);
 
@@ -64,4 +58,3 @@ export const getUser = async (
     }
   }
 };
-
